37af9f1eea9152d2409f4da6319cc9b63d694f56
Tom Lambert (Koopakiller) <tom.lambert.13@live.com>
2016-01-23 16:42:03 +0100

    Added Koopakiller.ClassDesktop.UnitTests project

---
+++ Koopakiller.ClassicDesktop.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Koopakiller.ClassicDesktop.UnitTests")]
+[assembly: AssemblyDescription("Koopakiller.ClassicDesktop.UnitTests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Koopakiller")]
+[assembly: AssemblyProduct("Koopakiller.ClassicDesktop.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Tom Lambert (Koopakiller) 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d075323-23d5-4e95-a505-b3aff3c4344b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
