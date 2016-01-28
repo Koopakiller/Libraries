5b1aa56cfea3635c292fd8fd291cba4e11b68fff
Tom Lambert (Koopakiller) <tom.lambert.13@live.com>
2016-01-28 20:49:44 +0100

    Implemented RingEnumerator + UnitTests

---
+++ Koopakiller.Portable.UnitTests/Linq/RingEnumeratorTests.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Koopakiller.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#pragma warning disable RECS0026 // Possible unassigned object created by 'new'
+// ReSharper disable ObjectCreationAsStatement
+
+namespace Koopakiller.Portable.UnitTests.Linq
+{
+    [TestClass]
+    public class RingEnumeratorTests
+    {
+        #region .ctor
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_EmptyEnumerator()
+        {
+            var enu = null as IEnumerator<int>;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            using (new RingEnumerator<int>(enu)) { }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_EmptyEnumerable()
+        {
+            var enu = null as IEnumerable<int>;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            using (new RingEnumerator<int>(enu)) { }
+        }
+
+        #endregion
+
+        #region EnumerateTest
+
+        [TestMethod]
+        public void EnumerateTest()
+        {
+            var arr = new[] { 1, 2, 3 };
+            using (var re = new RingEnumerator<int>(arr))
+            {
+                Assert.IsTrue(re.Take(9).SequenceEqual(new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3 }));
+            }
+        }
+
+        [TestMethod]
+        public void EnumerateTest_EmptySource()
+        {
+            var arr = new int[] { };
+            using (var re = new RingEnumerator<int>(arr))
+            {
+                Assert.IsFalse(re.Any());
+            }
+        }
+
+        [TestMethod]
+        public void EnumerateTest_WithCache()
+        {
+            var arr = new[] { 1, 2, 3 };
+            using (var re = new RingEnumerator<int>(arr) { UseCache = true })
+            {
+                Assert.IsTrue(re.Take(9).SequenceEqual(new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3 }));
+            }
+        }
+
+        [TestMethod]
+        public void EnumerateTest_EmptySource_WithCache()
+        {
+            var arr = new int[] { };
+            using (var re = new RingEnumerator<int>(arr) { UseCache = true })
+            {
+                Assert.IsFalse(re.Any());
+            }
+        }
+
+        #endregion
+
+        #region UseCache
+
+        [TestMethod]
+        public void UseCache_True()
+        {
+            using (var re = new RingEnumerator<int>(new StrangeTestEnumerator()) { UseCache = true })
+            {
+                Assert.IsTrue(re.Take(9).SequenceEqual(new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3 }));
+            }
+        }
+
+        [TestMethod]
+        public void UseCache_False()
+        {
+            using (var re = new RingEnumerator<int>(new StrangeTestEnumerator()))
+            {
+                Assert.IsTrue(re.Take(9).SequenceEqual(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void UseCache_DuringEnumeration()
+        {
+            var arr = new[] { 1, 2, 3 };
+            using (var re = new RingEnumerator<int>(arr))
+            {
+                re.MoveNext();
+                re.UseCache = true;
+            }
+        }
+
+        #endregion
+
+        #region Reset
+
+        [TestMethod]
+        public void Reset()
+        {
+            using (var re = new RingEnumerator<int>(new StrangeTestEnumerator()))
+            {
+                for (var i = 0; i < 5; ++i)
+                {
+                    re.MoveNext();
+                }
+                Assert.AreEqual(5, re.Current);
+                re.Reset();
+                re.MoveNext();
+                Assert.AreEqual(7, re.Current);//third enumeration, -> 3rd sub-array
+            }
+        }
+
+        [TestMethod]
+        public void Reset_WithCache()
+        {
+            using (var re = new RingEnumerator<int>(new StrangeTestEnumerator()) { UseCache = true })
+            {
+                for (var i = 0; i < 5; ++i)
+                {
+                    re.MoveNext();
+                }
+                Assert.AreEqual(2, re.Current);
+                re.Reset();
+                re.MoveNext();
+                Assert.AreEqual(4, re.Current);
+                //Reset resets the cache too, it will be rebuild and the second sub-array will be cached
+            }
+        }
+
+        #endregion
+
+        #region IEnumerator Current
+
+        [TestMethod]
+        public void IEnumerator_Current()
+        {
+            var arr = new[] { 1, 2, 3 };
+            using (var re = new RingEnumerator<int>(arr))
+            {
+                var enu = (IEnumerator)re;
+                var i = 0;
+                while (re.MoveNext())
+                {
+                    Assert.AreEqual(re.Current, enu.Current);
+                    ++i;
+                    if (i >= 10)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region IEnumerable GetEnumerator
+        
+        [TestMethod]
+        public void IEnumerable_GetEnumerator()
+        {
+            var arr = new[] { 1, 2, 3 };
+            using (var re = new RingEnumerator<int>(arr))
+            {
+                var i = 0;
+                var enu = ((IEnumerable)re).GetEnumerator();
+                while (enu.MoveNext())
+                {
+                    Assert.AreEqual(re.Current, enu.Current);
+                    ++i;
+                    if (i >= 10)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Represents an enumerator for an int-array which return 1,2,3 for the first 
+        /// enumeration and 4,5,6 after the first call of <see cref="Reset"/> and 7,8,9 
+        /// after the second call. Then it starts again with 1,2,3 and so on.
+        /// </summary>
+        private class StrangeTestEnumerator : IEnumerator<int>
+        {
+            private int _x;
+            private int _i = -1;
+            private readonly int[,] _arr =
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                ++this._i;
+                return this._i < this._arr.GetLength(0);
+            }
+
+            public void Reset()
+            {
+                this._i = 0 - 1;
+                ++this._x;
+                if (this._x >= this._arr.GetLength(1))
+                {
+                    this._x = 0;
+                }
+            }
+
+            public int Current => this._arr[this._x, this._i];
+
+            object IEnumerator.Current => this.Current;
+        }
+    }
+}
---
+++ Koopakiller.Portable/Linq/RingEnumerator.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Koopakiller.Linq
+{
+    public sealed class RingEnumerator<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        #region Fields
+
+        private readonly IEnumerator<T> _enumerator;
+
+        private bool _lastMoveNextWasFalse = true;
+        private bool _iterationStarted;
+        private bool _useCache;
+
+        private List<T> _cachedList;
+        private int _currentCachedItemIndex = -1;
+        private bool _cacheBuildComplete;
+
+        #endregion
+
+        #region .ctor
+
+        public RingEnumerator(IEnumerable<T> source) : this(source?.GetEnumerator()) { }
+
+        public RingEnumerator(IEnumerator<T> enumerator)
+        {
+            if (enumerator == null)
+            {
+                throw new ArgumentNullException(nameof(enumerator), $"{nameof(enumerator)} cannot be null.");
+            }
+            this._enumerator = enumerator;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool UseCache
+        {
+            get { return this._useCache; }
+            set
+            {
+                if (this._iterationStarted)
+                {
+                    throw new InvalidOperationException("The enumeration has already started.");
+                }
+                this._useCache = value;
+            }
+        }
+
+        #endregion
+
+        #region IEnumerator
+
+        object IEnumerator.Current => this.Current;
+
+        public T Current
+        {
+            get
+            {
+                if (this.UseCache && this._cacheBuildComplete)
+                {
+                    return this._cachedList[this._currentCachedItemIndex];
+                }
+                else
+                {
+                    return this._enumerator.Current;
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            this._currentCachedItemIndex = -1;
+            this._cacheBuildComplete = false;
+            this._iterationStarted = false;
+            this._enumerator.Reset();
+        }
+
+        public bool MoveNext()
+        {
+            if (!this._iterationStarted && this.UseCache)
+            {
+                this._cacheBuildComplete = false;
+                this._cachedList = new List<T>();
+            }
+
+            if (this.UseCache && this._cacheBuildComplete)
+            {
+                ++this._currentCachedItemIndex;
+                if (this._currentCachedItemIndex >= this._cachedList.Count)
+                {
+                    this._currentCachedItemIndex = 0;
+                }
+                return true;
+            }
+            else
+            {
+                this._iterationStarted = true;
+                if (this._enumerator.MoveNext())
+                {
+                    this._lastMoveNextWasFalse = false;
+                    if (this.UseCache)
+                    {
+                        this._cachedList.Add(this.Current);
+                    }
+                    return true;
+                }
+
+                if (this.UseCache)
+                {
+                    this._cacheBuildComplete = true;
+                    this._currentCachedItemIndex = 0;
+                    return this._cachedList.Count > 0;
+                }
+                else
+                {
+                    if (this._lastMoveNextWasFalse)
+                    {
+                        return false;
+                    }
+                    this._lastMoveNextWasFalse = true;
+                    this._enumerator.Reset();
+                    this._enumerator.MoveNext();
+                    return true;
+                }
+            }
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+            this._enumerator.Dispose();
+        }
+
+        #endregion
+
+        #region IEnumerable
+
+        public IEnumerator<T> GetEnumerator() => this;
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        #endregion
+    }
+}
